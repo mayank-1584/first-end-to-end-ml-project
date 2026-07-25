c16b1729b2df47af5246a253bc65e9ddcb699470
mayank-1584 <153091323+mayank-1584@users.noreply.github.com>
2026-07-25 23:10:14 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+hypen_e_dot='-e .'
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n" ,"") for req in requirements]
+        if hypen_e_dot in requirements:
+            requirements.remove(hypen_e_dot)
+    return requirements            
+
+
+setup(
+name='mlprojects',
+version='0.0.1',
+author='Mayank',
+author_email='mksharma6273.gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requiremnets.txt')
+
+
+)
