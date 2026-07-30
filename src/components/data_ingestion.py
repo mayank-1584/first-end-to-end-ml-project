9e8dff23aeb0e51029e6f81a73f15676b932c3ee
mayank-1584 <153091323+mayank-1584@users.noreply.github.com>
2026-07-30 22:39:48 +0530

    DATA_ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,4 +9,41 @@ from dataclasses import dataclass
 
 @dataclass 
 class dataingestionconfig:
-    train_data_path 
+    train_data_path = os.path.join('artifical' , "train.csv")
+    test_data_path = os.path.join('artifical' , "test.csv")
+    raw_data_path = os.path.join('artifical' , "data.csv")
+
+
+class Dataingestion:
+    def __init__(self):
+        self.ingestion_config=dataingestionconfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
+        try:
+            df=pd.read_csv('notebook\data\stud.csv')
+            logging.info("Read the dataset as data frame")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False,header=True)
+            logging.info("Train Test Split is initiated")
+
+            train_set,test_set=train_test_split(df,test_size=0.2 , random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True) 
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info("Ingesstion of the data is completed") 
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+
+
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+if __name__ =="__main__":
+    onj=Dataingestion()
+    onj.initiate_data_ingestion()   
