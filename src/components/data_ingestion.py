417e10f56b479d15a22dd444acf12ff42dbf926a
mayank-1584 <153091323+mayank-1584@users.noreply.github.com>
2026-07-31 22:39:05 +0530

    model trsined completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,7 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import datatransformationconfig
+from src.components.model_trainer import ModelTrainer 
 
 
 @dataclass 
@@ -53,4 +54,6 @@ if __name__ =="__main__":
 
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_tarnsformation(train_data,test_data) 
+    train_arr,test_arr,_= data_transformation.initiate_data_tarnsformation(train_data,test_data)
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
